1686a8ff7701de5a0735deb68b9321f8b21c0abd
Antonio Souza <47275237+Bartolomeuu@users.noreply.github.com>
2019-08-19 21:01:25 -0300

    arrumei medicina

--- Antonio/chat/app.js
+++ Antonio/chat/app.js
@@ -1,10 +1,11 @@
+//importanto 
 const express = require('express')
 const path = require('path')
-
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+//Caminhos
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
@@ -14,29 +15,33 @@ app.use('/', (req, res) => {
     res.send('index.html')
 })
 
-
+//Definindo Variaveis
 let sala = ''
 let messages = []
 
+
+//Conexão Socket
 io.on('connection', socket => {
     console.log(socket.id)
 
-
+    //Recebendo dados usuario
     socket.on('sala', data => {
         sala = data
         socket.join(sala);
         console.log(sala)
     })
 
+    //usuario esta digitando...press
     socket.on('typing', (dados) => {
         socket.broadcast.to(dados.sala).emit('renderTyping', dados.user)
     })
 
+    //usuario esta digitando...up
     socket.on('typingUp', (sala) => {
         socket.broadcast.to(sala).emit('noRenderTyping')
     })
 
-
+    //Enviando mensagem
     socket.on('sendMessage', data => {
         console.log(data)
         messages.push(data)
@@ -44,5 +49,5 @@ io.on('connection', socket => {
     })
 })
 
-
+//porta do servidor
 server.listen(3001)
